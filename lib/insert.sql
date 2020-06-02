585053f6cee3e7713524e67455896b1c55e3a6ac
Irene Scott <irenejinscott@gmail.com>
2020-06-02 05:04:04 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,3 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (12, 'Mr. Chocolate', 3, male, brown, angry, 0);
-INSERT INTO cats (id, name, age, gender, color, temperament, alive) VALUES (12, 'Mr. Chocolate', 3, male, brown, angry, 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (12, 'Mr. Chocolate', 3, male, brown, angry, 0);
 INSERT INTO cats (id, name, age, gender, color, temperament, alive) VALUES (12, 'Mr. Chocolate', 3, male, brown, angry, 0);
