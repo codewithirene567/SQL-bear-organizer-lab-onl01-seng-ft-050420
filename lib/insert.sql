22ea190f282ef2dac32a98e4d54bdc95e600d267
Irene Scott <irenejinscott@gmail.com>
2020-06-02 06:02:42 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'male', 'brown', 'angry', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'male', 'brown', 'goofy', 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 4, 'male', 'brown', 'angry', 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 6, 'female', 'brown', 'angry', 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 6, 'male', 'black', 'angry', 0);
